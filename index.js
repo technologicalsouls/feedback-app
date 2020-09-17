15f615fd69375f20e758f6898156150fbc61cc2f
technologicalsouls <technologicalsouls@gmail.com>
2020-09-17 00:41:06 -0400

    fin auth to start client

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+//execute
+require('./models/User');
+require('./services/passport');
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'hello world' });
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.listen(PORT);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+//can immediately call, pass in app obj
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('listening hello!!');
+});
